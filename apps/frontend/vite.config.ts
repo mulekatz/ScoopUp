7a11def46e1a3df7f7f6f7c1997f440092f9d9ab
mulekatz <mulekatz@gmail.com>
2025-02-09 16:11:32 +0100

    chore: update frontend build configuration for Vite and Turbo

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -6,15 +6,32 @@ import {nodePolyfills} from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), nodePolyfills()],
+  plugins: [react(), tailwindcss(),  nodePolyfills({
+    // Explizit angeben, welche Polyfills wir brauchen
+    include: ['buffer', 'process', 'util'],
+    globals: {
+      Buffer: true, // Buffer global verfügbar machen
+      process: true,
+      global: true
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      // Buffer-Polyfill explizit mappen
+      "buffer": "vite-plugin-node-polyfills/shims/buffer"
     },
   },
   build: {
+    outDir: "public",
+    emptyOutDir: true,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
+    rollupOptions: {
+      // Buffer als externes Modul behandeln
+      external: ['vite-plugin-node-polyfills/shims/buffer']
+    }
   },
 });
