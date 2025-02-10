ec2cbec133071f0fb6bfd149edd06e4ddd573a88
mulekatz <mulekatz@gmail.com>
2025-02-10 16:14:51 +0100

    fix: refine CORS configuration for receipt submission endpoint

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -46,12 +46,7 @@ export class App {
 
   private initializeMiddlewares() {
     this.app.use(morgan(LOG_FORMAT, { stream }));
-    this.app.use(
-      cors({
-        origin: ORIGIN,
-        credentials: CREDENTIALS,
-      }),
-    );
+    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
     this.app.use(hpp());
     this.app.use(
       helmet({
