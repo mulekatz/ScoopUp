6cf5f46c24997c723ce60067086ff8c35c71b5f0
mulekatz <mulekatz@gmail.com>
2025-02-11 17:52:03 +0100

    style: update styling of navigation

--- apps/frontend/src/components/navigation/Navigation.tsx
+++ apps/frontend/src/components/navigation/Navigation.tsx
@@ -8,18 +8,18 @@ import { Items } from "./Items";
 
 export default function Navigation() {
   return (
-    <div className="sticky bottom-0 flex justify-around items-center h-24 w-full">
+    <div className="sticky bottom-0 flex justify-around items-center py-3 lg:py-4 w-full text-sm">
       <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,hsla(0,100%,50%,0.2),transparent_1px),linear-gradient(to_bottom,hsla(0,100%,50%,0.2),transparent_1px)] bg-transparent backdrop-blur-lg bg-[size:.75rem_.75rem] [background-position:0.5rem_0.5rem]"></div>
-      <Items icon={<LiaHomeSolid size={36} />} path="/">
+      <Items icon={<LiaHomeSolid size={28} />} path="/">
         Home
       </Items>
-      <Items icon={<LiaMap size={36} />} path="/map">
+      <Items icon={<LiaMap size={28} />} path="/map">
         Map
       </Items>
-      <Items icon={<LiaPlusCircleSolid size={36} />} path="/add-bin">
+      <Items icon={<LiaPlusCircleSolid size={28} />} path="/add-bin">
         Add Bin
       </Items>
-      <Items icon={<LiaUserSolid size={36} />} path="/profile">
+      <Items icon={<LiaUserSolid size={28} />} path="/profile">
         Profile
       </Items>
     </div>
