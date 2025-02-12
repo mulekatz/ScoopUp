75ba20697c2b82d8f89a3d7b13e25c87c63a80cb
mulekatz <mulekatz@gmail.com>
2025-02-12 13:03:19 +0100

    style: add z-index to navigation for proper layering

--- apps/frontend/src/components/navigation/Navigation.tsx
+++ apps/frontend/src/components/navigation/Navigation.tsx
@@ -8,7 +8,7 @@ import { Items } from "./Items";
 
 export default function Navigation() {
   return (
-    <div className="sticky bottom-0 flex justify-around items-center py-3 lg:py-4 w-full text-sm">
+    <div className="sticky bottom-0 flex justify-around items-center py-3 lg:py-4 w-full text-sm z-20">
       <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,hsla(0,100%,50%,0.2),transparent_1px),linear-gradient(to_bottom,hsla(0,100%,50%,0.2),transparent_1px)] bg-transparent backdrop-blur-lg bg-[size:.75rem_.75rem] [background-position:0.5rem_0.5rem]"></div>
       <Items icon={<LiaHomeSolid size={28} />} path="/">
         Home
